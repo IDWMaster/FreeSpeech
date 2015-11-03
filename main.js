c129ec3ee98c99d63cd32da2bd49e1f5901d73e8
Brian Bosak <webadm@elcnet.servehttp.com>
2015-11-03 12:57:17 -0600

    Updated to new server architecture

--- main.js
+++ main.js
@@ -6,10 +6,5 @@ var httpserver = require('openserver');
 
 var server = httpserver.startServer({ip:'::',port:8080});
 server.RegPath('/', function(request,response){
-	server.renderHtml('index.html', function(html) {
-		response.writeHead(200, {'Content-Type':'text/html'});
-		response.write(html);
-		
-		response.end();
-	});
+	response.respondWithHtml('index.html');
 });
