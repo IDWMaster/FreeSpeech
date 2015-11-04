2bca15338a622b96a5740c318654b031c59daec0
Brian Bosak <webadm@elcnet.servehttp.com>
2015-11-04 10:34:13 -0600

    Updated example site

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 var httpserver = require('openserver');
 
 
-
+var datamodel = {
+		pages:{'Home page':'/','Async request demo':'/asyncdemo'}
+};
 
 
 var server = httpserver.startServer({ip:'::',port:8080});
 server.RegPath('/', function(request,response){
+	server.setModel(datamodel);
 	response.respondWithHtml('index.html');
+});
+server.RegPath('/asyncdemo',function(request,response){
+	server.setModel(datamodel);
+	response.respondWithHtml('asyncdemo.html');
 });
