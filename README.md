4ca56eb37ec83b8290d9e646ae905a568da258fd
Brian Bosak <webadm@elcnet.servehttp.com>
2015-12-07 20:09:36 -0600

    Misc. changes

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 
 
-# testwebsite
+# Free Speech Project
 
 
+The Free Speech Project is a collection of technologies making up a secure, P2P, encrypted, and (optionally) anonymous Internet. It is comprised of a series of NPM packages, and is currently a work-in-progress. The goal of this project is to make it easy for everyone to embed secure communications tools into their application, and later; to provide a web-based user interface for encrypted chat and social networking features.
 
-## Usage
+## Installation
+Download the GitHub repository at https://github.com/IDWMaster/FreeSpeech, and run npm install to install dependency packages.
+
+## Running
+
+To run, type the command: node main.js from the root directory of the FreeSpeech project.
 
 
 
 ## Developing
+See https://github.com/IDWMaster/FreeSpeech
 
 
 
-### Tools
-
-Created with [Nodeclipse](https://github.com/Nodeclipse/nodeclipse-1)
- ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
-
-Nodeclipse is free open-source project that grows with your contributions.
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 /**
  * IMMEDIATE TODO LIST
+ * ASAP: Refactor this code into multiple NodeJS modules
+ * For example; the various Session components should be refactored.
+ * 
  * Make sure that clients can authenticate to first-hop servers.
  * For clients advertising a thumbprint; the thumbprint and public key should be stored in the local database.
  * For clients not supplying a thumbprint; no information whatsoever should be recorded with regards to the connection.
@@ -17,7 +20,7 @@ var mongo = require('mongodb');
 var uuid = require('node-uuid');
 var dgram = require('dgram');
 var NodeRSA = require('node-rsa');
-
+var Session = require('freespeech-session').Session;
 var UDPClient;
 
 
@@ -176,151 +179,6 @@ var EncryptionKeys = {
     }
 };
 
-/**
- * @class
- */
-var Session = function () {
-    var sessionID;
-    if(Session.available.length>0) {
-        sessionID = Session.available.pop();
-    }else {
-        sessionID = Session.CID;
-        Session.CID++;
-    }
-    
-    var callbacks = new Array();
-    
-    
-    
-    var reassemblyBuffer = new Object();
-    
-    var currentPacketID = 0;
-    
-    var Protected = {};
-    var retval = {
-        send: function (data) {},
-        /**
-         * Registers a callback which is invoked when a packet is received
-         */
-        registerReceiveCallback: function (callback) {
-            return callbacks.push(callback) - 1;
-        },
-        /**
-         * Unregisters a callback
-         */
-        unregisterReceiveCallback: function (id) {
-            callbacks.splice(id, 1);
-        },
-        /**
-         * Subclasses this instance
-         */
-        subclass: function (callback) {
-            callback(Protected);
-            return this;
-        },
-                close: function () {
-                    Session.available.push(sessionID);
-                },
-                /**
-                 * Encodes and transmits a packet, fragmenting it if necessary
-                 * @param {Buffer} data
-                 * @returns {undefined}
-                 */
-                sendPacket:function(data) {
-                    //TODO: Encode in general packet format
-                    var packetOffset = 0;
-                    var mlen = Math.min(data.length-packetOffset,4096);
-                        var i = 0;
-                    while(data.length-packetOffset>0) {
-                        
-                        var send = function(packet,i) {
-                        var buffy = new Buffer(4+1+2+2+4+packet.length);
-                            buffy.writeUInt32LE(currentPacketID,0);
-                            buffy[4] = 2;
-                            buffy.writeUInt16LE(sessionID,4+1);
-                            buffy.writeUInt16LE(i,4+1+2);
-                            buffy.writeUInt32LE(packet.length,4+1+2+2);
-                            packet.copy(buffy,4+1+2+2+4);
-                            retval.send(buffy);
-                       
-                    };
-                    var mb = new Buffer(mlen);
-                    data.copy(mb,0,0,mlen);
-                    send(mb,i);
-                        packetOffset+=mlen;
-                        i++;
-                    }
-                    
-                        currentPacketID++;
-                },
-                /**
-                 * Decodes a packet
-                 * @param {Buffer} data
-                 * @returns {undefined}
-                 */
-                decodePacket:function(data) {
-                    try {
-                    if(data[4] == 2) {
-                        var messageID = data.readUInt32LE(0);
-                        var _sessionID = data.readUInt16LE(4+1);
-                        var packetID = data.readUInt16LE(4+1+2);
-                        if(sessionID != _sessionID) {
-                            return;
-                        }
-                        var dlen = data.readUInt32LE(4+1+2+2);
-                        if(!reassemblyBuffer[messageID]) {
-                            var mray = new Array(Math.ceil(dlen/4096));
-                            mray.buffer = new Buffer(dlen);
-                            mray.currentLength = 0;
-                            reassemblyBuffer[messageID] = mray;
-                        }
-                        var cBuffer = reassemblyBuffer[messageID];
-                        if(cBuffer[packetID]) {
-                            return;
-                        }
-                        var dSegLen = Math.min(dlen-cBuffer.currentLength,data.length-(4+1+2+2));
-                        cBuffer.currentLength+=dSegLen;
-                        cBuffer[packetID] = true;
-                        data.copy(cBuffer.buffer,4096*packetID,4+1+2+2+4,4+1+2+2+4+dSegLen);
-                        if(cBuffer.currentLength >= dlen) {
-                            //We have a packet!
-                            reassemblyBuffer[messageID] = null;
-                            Protected.ntfyPacket(cBuffer.buffer);
-                        }
-                    }
-                }catch(er) {
-                    
-                }
-                },
-                /**
-                 * Gets the current session identifier
-                 * @returns {Number|Session.CID|Object}
-                 */
-                getSessionID:function() {
-                    return sessionID;
-                },
-                /**
-                 * Sets the current session ID for a remote session
-                 * @param {Number} remoteID
-                 * @returns {undefined}
-                 */
-                setSessionID:function(remoteID) {
-                    Session.available.push(remoteID);
-                    sessionID = remoteID;
-                    retval.setSessionID = function(id) {
-                        throw 'This function can only be called once.';
-                    };
-                }
-    };
-    Protected.ntfyPacket = function (packet) {
-        for (var i = 0; i < callbacks.length; i++) {
-            callbacks[i](packet);
-        }
-    };
-    return retval;
-};
-Session.CID = 0;
-Session.available = new Array();
 
 
 /**
